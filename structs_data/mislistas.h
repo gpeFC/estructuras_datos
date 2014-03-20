f72a60be7066fc5e8c855d89eb99c0e98414aceb
Emanuel <gpe_fc@uaem.mx>
2014-03-19 22:40:30 -0600

    Practica usando colas implementadas con listas enlazadas

---
+++ structs_data/mislistas.h
@@ -0,0 +1,8 @@
+/******************************************************************************
+* Estructura de Datos
+* Emanuel GP
+* 19 / Marzo / 2014
+* 
+* Practica: Aplicaciones de LE y Queues(Funciones y procedimientos/TAD Queue).
+******************************************************************************/
+
---
+++ structs_data/queues.c
@@ -0,0 +1,13 @@
+/******************************************************************************
+* Estructura de Datos
+* Emanuel GP
+* 19 / Marzo / 2014
+* 
+* Practica: Aplicaciones de LE y Queues.
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void){
+}
