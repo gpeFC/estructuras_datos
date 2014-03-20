30775ee8e3c940c6acb9f45b3cedfbf340ca48dc
Emanuel <gpe_fc@uaem.mx>
2014-03-20 02:03:33 -0600

    Practic actualizada

--- structs_data/mislistas.h
+++ structs_data/mislistas.h
@@ -6,3 +6,84 @@
 * Practica: Aplicaciones de LE y Queues(Funciones y procedimientos/TAD Queue).
 ******************************************************************************/
 
+typedef struct Nodo{
+	char etiqueta;
+	int numero;
+	struct Nodo *sigNodo;
+}tsNODO;
+
+void actualizaCaja(){}
+
+void insercionLista(tsNODO **lista, char etq, int num){
+	tsNODO *nodo;
+	nodo = (tsNODO*)malloc(sizeof(tsNODO));
+	nodo->etiqueta = etq;
+	nodo->numero = num;
+	nodo->sigNodo = NULL;
+	if(*lista == NULL)
+		*lista = nodo;
+	else{
+		nodo->sigNodo = *lista;
+		*lista = nodo;
+	}
+}
+
+void insercionQueue(tsNODO **cabeza, tsNODO **cola, char etq, int num){
+	tsNODO *nodo;
+	nodo = (tsNODO*)malloc(sizeof(tsNODO));
+	nodo->etiqueta = etq;
+	nodo->numero = num;
+	nodo->sigNodo = NULL;
+	if(*cabeza == NULL){
+		*cabeza = nodo;
+		*cola = nodo;
+	}
+	else{
+		(*cola)->sigNodo = nodo;
+		*cola = nodo;
+	}
+}
+
+void extraccionQueue(tsNODO **cabeza, char *etq, int *num){
+	tsNODO *nodo;
+	if(*cabeza == NULL){
+		*etq = '0';
+		*num = 0;
+	}
+	else{
+		nodo = *cabeza;
+		*cabeza = (*cabeza)->sigNodo;
+		*etq = nodo->etiqueta;
+		*num = nodo->numero;
+		free(nodo);
+	}
+}
+
+void mostrarLista(tsNODO *lista){
+	tsNODO *nodo;
+	nodo = lista;
+	while(nodo != NULL){
+		printf("|Caja[%d]:Cliente[%c]|\t", nodo->numero, nodo->etiqueta);
+		nodo = nodo->sigNodo;
+	}
+}
+
+void mostrarQueue(tsNODO *cabeza){
+	tsNODO *nodo;
+	nodo = cabeza;
+	while(nodo != NULL){
+		printf("[Cliente(%c):Delay(%d)] ", nodo->etiqueta, nodo->numero);
+		nodo = nodo->sigNodo;
+	}
+}
+
+void vaciarLista(tsNODO **lista){
+	tsNODO *nodo;
+	nodo = *lista;
+	while(nodo != NULL){
+		*lista = (*lista)->sigNodo;
+		free(nodo);
+		nodo = *lista;
+	}
+}
+
--- structs_data/queues.c
+++ structs_data/queues.c
@@ -8,6 +8,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "mislistas.h"
 
 void main(void){
+	int i, num, item=65;
+	tsNODO *cabezaQ=NULL, *colaQ=NULL;
+	tsNODO *lista=NULL;
+
+	srand(time(NULL));
+
+	num = rand() % 2+3;
+	for(i=0;i<num;i++){
+		insercionLista(&lista, 'Z', i+1);
+	}
+	num = rand() % 14+5;
+	for(i=0;i<num;i++){
+		insercionQueue(&cabezaQ, &colaQ, item+i, rand()%7+3);
+	}
+
+	printf("\n");
+	mostrarLista(lista);
+	printf("\n");
+
+	printf("\n{ ");
+	mostrarQueue(cabezaQ);
+	printf("}\n");
+
+	do{
+		system("clear");
+
+		printf("\n");
+		mostrarLista(lista);
+		printf("\n");
+
+		printf("\n{ ");
+		mostrarQueue(cabezaQ);
+		printf("}\n");
+
+		
+
+	}while(*colaQ != NULL);
 }
