c626275b7d1bfc190d7065a2c03ce28ad4fce915
Emanuel <gpe_fc@uaem.mx>
2014-04-08 04:08:54 -0500

    Nueva practica: Polinomio

---
+++ structs_data/polinomio.c
@@ -0,0 +1,13 @@
+/******************************************************************************
+* Estructura de Datos
+* Emanuel GP
+* 25 / Febrero / 2014
+* 
+* Practica: Estructura de Datos( Lista Enlazada Circular).
+******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void){
+}
